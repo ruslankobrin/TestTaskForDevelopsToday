57e7e9f2a616d26fdf738d7d65c4f0d25dac0158
ruslankobrin <ruslankobrin@gmail.com>
2020-05-15 21:41:51 +0300

    add Post and Comment models

--- news/models.py
+++ news/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    link = models.URLField(null=True, blank=True)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    content = models.CharField(max_length=400)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
